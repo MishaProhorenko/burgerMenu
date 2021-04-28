0a5279c5c7d55d37e7e5262a649ce6844218d9fc
Mishaproh <mishaprohorenko96@gmail.com>
2021-04-28 10:37:06 +0300

    smooth scroll

--- js/burger.js
+++ js/burger.js
@@ -1,1 +1,70 @@
 'use strict'
+
+// Определяем устройство на котором открт сайт
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    IOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera mini/i);
+    },
+    Window: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.IOS() ||
+            isMobile.Opera() ||
+            isMobile.Window());
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    if (menuArrows.length > 0) {
+        for (let i = 0; i < menuArrows.length; i++) {
+            const menuArrow = menuArrows[i];
+            menuArrow.addEventListener('click', function (e) {
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+
+} else {
+    document.body.classList.add('_pc');
+}
+
+
+//Прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top:gotoBlockValue,
+                behavior:"smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
